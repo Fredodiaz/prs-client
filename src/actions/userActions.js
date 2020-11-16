ed35d2f7c5cf3b5faace0961a3e80291037ee217
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-16 14:06:57 -0800

    feat(actions): added user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,21 @@
 // TYPES
-import { SET_USER_NAME } from './index'
+import { SET_CURRENT_ROOM, SET_JOINED_GAME, SET_USER_NAME } from './index'
+
+/* Sets Room User Joined */
+export const setCurrentRoom = (roomName) => dispatch => {
+    dispatch({
+        type: SET_CURRENT_ROOM,
+        payload: roomName
+    })
+}
+
+/* Sets Room User Joined */
+export const setJoinedGame = (hasJoinedLobby) => dispatch => {
+    dispatch({
+        type: SET_JOINED_GAME,
+        payload: hasJoinedLobby
+    })
+}
 
 export const setUserName = (username) => dispatch => {
     dispatch({
@@ -7,3 +23,4 @@ export const setUserName = (username) => dispatch => {
         payload: username
     })
 }
+
