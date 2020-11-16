5ed376f56c35dc6ff8ce02cb4d08d7f13733a931
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-16 14:07:16 -0800

    feat(reducers): added user reducers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,17 +1,30 @@
 // Types
-import { SET_USER_NAME } from '../actions'
+import { SET_CURRENT_ROOM, SET_JOINED_GAME, SET_USER_NAME } from '../actions'
 
 const initialState = {
-    name: ''
+    currentRoom: '',
+    hasJoinedGame: false,
+    name: '',
 }
 
 const userReducers = (state=initialState, action) => {
     switch(action.type) {
-        case SET_USER_NAME:
-            console.log("NAME", action.payload)
+        case SET_CURRENT_ROOM:
+            console.log('SET_CURRENT_ROOM', action.payload)
             return {
+                ...state,
+                currentRoom: action.payload,
+            }
+        case SET_JOINED_GAME:
+            return {
+                ...state,
+                hasJoinedGame: action.payload
+            }
+        case SET_USER_NAME:
+            console.log('SET_USER_NAME', action.payload)
+            return {
+                ...state,
                 name: action.payload,
-                ...state
             }
         default:
             return state;
