f111750de7680116e619ada9b3f7d5d613f0194c
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-23 12:50:58 -0800

    feat(host): added host logic

--- src/components/pages/host/Host.js
+++ src/components/pages/host/Host.js
@@ -2,21 +2,24 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+// Redux
+import { connect } from 'react-redux'
+
 // Components
 import Players from '../lobby/playerslist/PlayersList'
-// import Results from '../results/Results'
 
 // Style
 import css from './host.module.css'
 
-const Host = () => {
+const Host = (props) => {
+    const { game } = props
+
     const [ GAMECODE, SETGAMECODE ] = useState(12345)
 
     useEffect(() => {
         window.IO.on('gameCode', handleGameCode)
+        window.IO.on('startedGame')
         window.IO.emit('hostGame');
-        // setStartedSession(true)
-        
     }, [])
 
     const handleGameCode = code => {
@@ -27,6 +30,14 @@ const Host = () => {
         window.IO.emit('hostLeaveGame', GAMECODE)
     }
 
+    const handleHostStartGame = () => {
+        if(game.players.length < 2) {
+            alert('Must need at least two players to start the game')
+        } else {
+            window.IO.emit('hostStartedGame');
+        }
+    }
+
     return (
         <div className={css.host_wrap}>
             {/* {hasStartedGame ? <Results /> :  */}
@@ -35,14 +46,17 @@ const Host = () => {
                 <h1 className={css.header}>Game Code: {GAMECODE}</h1>
                 <Players />
             </div>
-            <h4 style={{marginTop: '30px'}}>Start Game</h4>
+            <h4 className={css.start_game} onClick={() => handleHostStartGame()}>Start Game</h4>
             <Link onClick={handleHostLeave} style={{textDecoration: 'none'}} to={'/'}>
                 <h4>Go Back</h4>
             </Link>
             <p className={css.warning}>(Warning: "Go Back" will remove all players from this match)</p>
         </div>
-
     )
 }
 
-export default Host
+const mapStateToProps = state => ({
+    game: state.game
+})
+
+export default connect(mapStateToProps, {})(Host)
