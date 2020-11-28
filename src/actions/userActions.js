742e95003b72a301775b5632d37ed7a7efe73ca4
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-28 11:59:08 -0800

    feat(actions): added user current choice

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 // TYPES
-import { SET_CURRENT_ROOM, SET_IS_IN_GAME, SET_JOINED_GAME, SET_USER_NAME, SET_USER_OPPONENT } from './index'
+import { SET_CURRENT_CHOICE, SET_CURRENT_ROOM, SET_IS_IN_GAME, SET_JOINED_GAME, SET_USER_NAME, SET_USER_OPPONENT } from './index'
 
 /* Sets Room User Joined */
 export const setCurrentRoom = (roomName) => dispatch => {
@@ -35,5 +35,12 @@ export const setIsUserInGame = (isUserInGame) => dispatch => {
     dispatch({
         type: SET_IS_IN_GAME,
         payload: isUserInGame
+    })
+}
+
+export const setCurrentChoice = (choice) => dispatch => {
+    dispatch({
+        type: SET_CURRENT_CHOICE,
+        payload: choice
     })
 }
