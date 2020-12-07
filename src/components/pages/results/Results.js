ac73052584c82f32438cf88d5bd62966c2deb6c5
Fredodiaz <alfredo.diazavina@gmail.com>
2020-12-07 10:56:21 -0800

    workaround(resutls): display winner/loser

--- src/components/pages/results/Results.js
+++ src/components/pages/results/Results.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 // Redux
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 // Styles
 import css from './results.module.css'
@@ -10,6 +11,7 @@ import css from './results.module.css'
 const Results = (props) => {
     const { user } = props
 
+    // eslint-disable-next-line
     const [ name, setName ] = useState('')
 
     useEffect(() => {
@@ -18,20 +20,11 @@ const Results = (props) => {
         })
     }, [])
 
-    const renderTempVal = () => {
-        return Math.floor(Math.random()*(999-100+1)+100);
-    }
-
     return (
         <div className={css.results_wrap}>
-            {user.hasWon ? <h1 className={css.temp}>You are the winner!</h1> : null}
-            <h1 className={css.temp}>Winning Match</h1>
-            <h6>{renderTempVal()} vs {renderTempVal()}</h6>
-            <h1 className={css.temp}>Ongoing Battles</h1>
-            <h6>{renderTempVal()} vs {renderTempVal()}</h6>
-            <h1 className={css.temp}>Matches{renderTempVal()} Done</h1>
-            <h6>{renderTempVal()} vs {renderTempVal()}</h6>
-            {name !== '' ? <h1 className={css.temp}>{name} is the winner</h1> : null}
+            {user.hasWon ? <h1 className={css.temp}>You are the Winner!</h1> : null}
+            {!user.hasWon ? <h1 className={css.temp}>You were eliminated, thank you for playing!</h1> : null}
+            {!user.isInGame ? <Redirect to={'/lobby'}/> : null}
         </div>
     )
 }
